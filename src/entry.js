b6103596bdad39363acf796d3863ff25e76573e0
Vimal Prasad <vimalprasad@Vimals-MacBook-Air.local>
2023-09-06 11:04:55 +0530

    Fixed Style Issues

--- src/entry.js
+++ src/entry.js
@@ -4,4 +4,4 @@ import './assets/js/shared/misc.js'
 import './assets/js/shared/off-canvas.js'
 
 import './assets/scss/demo_1/style.scss'
-import './assets/scss/shared/style.scss'
+// import './assets/scss/shared/style.scss'
